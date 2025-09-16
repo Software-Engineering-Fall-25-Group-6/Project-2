da80d3cdcb06f442d6b44547015eb6e3296704e0
Elwood <elwoodcarroll@icloud.com>
2025-09-16 10:41:06 -0400

    Finished make-multi-filter
    Says it works with the provided test

--- make-multi-filter.js
+++ make-multi-filter.js
@@ -1,8 +1,26 @@
+'use strict';
+
 function MakeMultiFilter(originalArray){
-    let currentArray = originalArray;
-    return function(filterCriteria, callback){
-        if(filterCriteria === undefined){
-            return currentArray;
+
+    const arrayFilterer = function(filterCriteria, callback){
+        if(arrayFilterer.currentArray === undefined){
+            arrayFilterer.currentArray = originalArray;
         }
+        if(filterCriteria === undefined){
+            return arrayFilterer.currentArray;
+        }
+
+        if(typeof filterCriteria === "function"){
+            arrayFilterer.currentArray = arrayFilterer.currentArray.filter(filterCriteria);
+        }
+
+        if(typeof callback === "function"){
+            callback.call(originalArray, arrayFilterer.currentArray);
+        }
+        
+
+        return arrayFilterer;
     }
+
+    return arrayFilterer;
 }
