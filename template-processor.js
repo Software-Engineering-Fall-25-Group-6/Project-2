fcd9e28fd79b88c9f06532d80438562962ff602f
cjgalla <collinjosephgallagher05@gmail.com>
2025-09-16 10:47:07 -0400

    Update template-processor.js

--- template-processor.js
+++ template-processor.js
@@ -1,1 +1,10 @@
+function TemplateProcessor(template) {
+    this.template = template;
+}
+
+TemplateProcessor.prototype.fillIn = function (dictionary) {
+    return this.template.replace(/{{(.*?)}}/g, function(match, prop) {
+        return dictionary.hasOwnProperty(prop) ? dictionary[prop] : '';
+    });
+};
 
